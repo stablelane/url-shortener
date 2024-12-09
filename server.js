a601c2695f39762cf5dd7d0988e8eb53658131ac
stablelane <aaanshad09@gmail.com>
2024-12-09 21:23:08 +0530

    add basic layout and functions

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+const ShortUrl = require('./models/shortUrl')
 
+mongoose.connect('mongodb://localhost/urlShortner')
 
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false}))
 
+app.get('/', async (req, res) => {    
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls: shortUrls})
+})
+
+app.post('/shortUrl', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl })
+    res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+    const Url = await ShortUrl.findOne({ short: req.params.shortUrl })
+    Url.clicks++
+    Url.save()
+    res.redirect(Url.full)
+})
 
 app.listen(process.env.PORT || 5000);
